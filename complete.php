547f5108e40e5cbc921d3d478a4d4c1a6a739f08
gerrykiboma <92923811+gerrykiboma@users.noreply.github.com>
2022-06-29 00:09:02 +0300

    Update complete.php

--- complete.php
+++ complete.php
@@ -1,16 +1,24 @@
 <?php
-//get data from form  
 $name = $_POST['name'];
-$email= $_POST['email'];
-$message= $_POST['message'];
-$to = "gerrykiboma@gmail.com";
-$subject = "Mail From website";
-$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: noreply@yoursite.com" . "\r\n" .
-"CC: somebodyelse@example.com";
-if($email!=NULL){
-    mail($to,$subject,$txt,$headers);
-}
-//redirect
-header("Location:thankyou.html");
+$Visitor_email= $_POST['email'];
+$message = $_POST['message'] ; 
+
+$email_from= 'nyarangigerry@gmail.com';
+
+$email_subject ="New form submission";
+
+$email_body ="User Name: $name.\n".
+              "User Email: $Visitor_email.\n".
+              "User message: $message.\n";
+
+$to = "gerrykiboma@gmail.com"
+
+$headers = "From: $email_from \r\n";
+
+$headers ="Reply-To:$Visitor_email \r\n";
+
+mail(sto,$email_subject,$email_body,$headers);
+
+header("Location: index.html");
+
 ?>
